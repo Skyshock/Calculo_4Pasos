8c83f0c17db1b1e4e2a8ad25b5539fb9bd8448cc
greghernandez <gregoryarthur13@gmail.com>
2019-04-09 08:13:50 -0500

    Start

--- Main.py
+++ Main.py
@@ -1,1 +1,4 @@
-print('No funciona')
+# Inserte titulo y descripcion pro en ingles y en español por favor#
+print('-----------------------------------------------')
+print('-Solucion de derivadas por el metodo de 4 pasos-')
+print('-----------------------------------------------')
