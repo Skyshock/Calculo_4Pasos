f2e55efaa515d5883cbdf6f66a280d4c7f058e23
greghernandez <gregoryarthur13@gmail.com>
2019-04-09 08:54:55 -0500

    Declaracion de pasos a seguir

--- Main.py
+++ Main.py
@@ -1,4 +1,13 @@
-# Inserte titulo y descripcion pro en ingles y en español por favor#
+#Inserte titulo y descripcion pro en ingles y en espanol por favor
 print('-----------------------------------------------')
 print('-Solucion de derivadas por el metodo de 4 pasos-')
 print('-----------------------------------------------')
+
+#Solicitamnos la ecuacion
+
+#Paso 1: Sumamos h a x para formar f(x+h)
+#Paso 2:  Restamos a f(x+h) la ecuacion original para tener --> f(x+h)-f(x)
+#Paso 3: Dividimos entre h --> f(x+h)-f(x)/h
+#Paso 4: obtenemos el limite cuando h tiende a o
+
+#Imprimimos el resultado
